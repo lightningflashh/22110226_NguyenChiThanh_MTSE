2730330bd995ad026e779c25650d88f0d802bd4c
lightningflashh <thanhchi0429@gmail.com>
2025-11-05 10:15:03 +0700

    exercise 01 11/5/2025

---
+++ nct-profile/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="text-center mt-20 font-sans">
+      <h1 className="text-3xl font-bold text-red-600">Thông tin cá nhân</h1>
+      <p className="mt-4 text-lg">Họ và tên: Nguyễn Chí Thanh</p>
+      <p>MSSV: 22110226</p>
+      <p>Lớp: 22110CLST01</p>
+      <p>Email: 22110226@student.hcmute.edu.vn</p>
+    </div>
+  );
+}
+
+export default App;
